ade2787e1cad7f806e6558dd7421dd62ee3b34a6
liupenghao <liupenghao1205@163.com>
2018-07-11 18:52:54 +0800

    滑动

--- js/main.js
+++ js/main.js
@@ -9,24 +9,16 @@ var iscrollUls = document.getElementsByClassName('iscrollUl');
 for (var i=0;i<iscrollUls.length;i++) {
   var lists = iscrollUls[i].children;
   var ulWidth = 0;
-  for(var j=0;j<lists.length;j++){
+  for (var j = 0; j < lists.length; j++) {
     var liWidth = lists[j].offsetWidth;
     // var marginLeft = window.getComputedStyle(lists[i], null).getPropertyValue('margin-left');
     // var marginRigit= window.getComputedStyle(lists[i], null).getPropertyValue('margin-right');
     // width += liWidth+marginLeft+marginRigit;
     ulWidth += liWidth
   }
-  console.log('iscrollUls[i]',iscrollUls[i])
-  iscrollUls[i].style.width = ulWidth+'px';
 }
 
-
-
-var pics = document.getElementsByClassName('activity-container');
-for(var i=0;i<pics.length;i++) {
-  new IScroll(pics[i],{
-    scrollX:true,
-    scrollY:false,
-    click:true
-  });
-}
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  freeMode: true,
+});
